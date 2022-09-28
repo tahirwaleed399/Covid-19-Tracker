d98d992858ced06d2239dd968e6406e8cb856325
Muhammad Waleed Tahir <73441359+tahirwaleed399@users.noreply.github.com>
2022-09-28 16:09:23 +0500

    Inital Commit

---
+++ src/app/Components/dash-cards/dash-cards.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DashCardsComponent } from './dash-cards.component';
+
+describe('DashCardsComponent', () => {
+  let component: DashCardsComponent;
+  let fixture: ComponentFixture<DashCardsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DashCardsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashCardsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountriesComponent } from './Components/countries/countries.component';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component:HomeComponent
+},
+{
+  path:'countries',
+  component:CountriesComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
